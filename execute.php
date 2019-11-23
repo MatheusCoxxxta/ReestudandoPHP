1c304e227ccc4de48fede4f8951660d1eb9b46e8
Matheus Costa <matheus271200.ms@gmail.com>
2019-11-23 20:38:44 -0300

    Adicionando função de data

--- execute.php
+++ execute.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Portfólio </title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1> Bem vindo</h1>
+		
+		
+		<p> Descubra qual a data de hoje: 
 <?php
-	echo "Isso será executado";
+		$btnSubmit = $_POST["btnSubmit"];    
+		if($btnSubmit == "Que dia é hoje?") echo date("d/m/Y");
+		else "Erro :(";
 ?>
+		</p>
+	</body>
+</html>
